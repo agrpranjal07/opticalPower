b319bcf68a8ba9c39728ab87d883a9543c066880
agrpranjal07 <myselfpranjal2005@gmail.com>
2025-01-15 16:28:34 +0530

    Page for basic first testing 3

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 
-let i=30;
+let i=50;
 let j=3;
 let c=0;
 document.write("<div style=' margin:20px; display:grid; grid-template-columns: repeat(5, 1fr); '>");
-while(i>=8){
+while(i>=10){
     if(c==5){
-        i-=2;
+        i-=1;
         c=0;
     }
     document.write("<span style='margin:20px auto; font-size:"+i+"px'>"+String.fromCharCode(65+j)+" "+i+"</span>");
